8b6fc3c4504efd4e195f244203573f9b31d92117
shouvik007 <68604703+shouvik007@users.noreply.github.com>
2026-04-08 15:35:45 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>m;
+        if(s.size()!=t.size()){
+            return false;
+        }
+        for(int i=0;i<s.size();i++) {
+            m[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++) {
+            m[t[i]]--;
+        }
+        for(auto x:m){
+            if(x.second>0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
