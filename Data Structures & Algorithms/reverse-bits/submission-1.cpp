b7b2aae4d289d6f48f3b9ac7aabaa2af42465c83
shouvik007 <68604703+shouvik007@users.noreply.github.com>
2026-05-06 18:22:03 +0530

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
+        for(int shift = 31; shift>=0;shift --){
+                uint32_t bit = n&1;
+                res = res|(bit<<shift);
+                n=n>>1;
+        }
+        return res;
+    }
+};
