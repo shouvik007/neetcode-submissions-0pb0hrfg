5895ad60ac07af65f1ca6373b7a318aca0ea4fc5
shouvik007 <68604703+shouvik007@users.noreply.github.com>
2026-05-15 15:46:05 +0530

    Add: word-ladder - submission-0

---
+++ Data Structures & Algorithms/word-ladder/submission-0.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool isSame(string& word1, string& word2  ){
+        if(word1.size() != word2.size()){
+            return false;
+        }
+        int i =0;int j =0;
+        int count = 0;
+        while(i<word1.size() && j<word2.size()){
+            if(word1[i]!=word2[j]){
+                count++;
+            }
+            i++;
+            j++;
+        }
+        if(count == 1){
+            return true;
+        }
+        return false;
+    }
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        vector<vector<int>>v(wordList.size());
+        int n = wordList.size();
+        for(int i =0;i<n;i++){
+            for(int j =i+1;j<n;j++){
+                    if(isSame(wordList[i], wordList[j])){
+                        v[i].push_back(j);
+                        v[j].push_back(i);
+                    }
+            }
+        }
+        queue<pair<int, int>> q; // {index, distance}
+        vector<bool> vis(n, false);
+        for(int i = 0; i < n; i++) {
+            if(isSame(beginWord, wordList[i])) {
+                q.push({i, 2});
+                vis[i] = true;
+            }
+        }
+        if (beginWord == endWord) return 1;
+        while(!q.empty()) {
+            auto [curr, dist] = q.front();
+            q.pop();
+            if(wordList[curr] == endWord) return dist;
+            for(int neighbor : v[curr]) {
+                if(!vis[neighbor]) {
+                    vis[neighbor] = true;
+                    q.push({neighbor, dist + 1});
+                }
+            }
+        }
+        return 0;
+    }
+
+};
